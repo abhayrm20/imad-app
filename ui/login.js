a6a3a1e3d0b5d39010b41870649bd977612fad72
abhayrm20 <abhayrm20@gmail.com>
2017-08-05 21:07:56 +0530

    Create login.js

---
+++ ui/login.js
@@ -0,0 +1,19 @@
+function check(form) { /*function to check userid & password*/
+                /*the following code checkes whether the entered userid and password are matching*/
+                if(form.uname.value == "admin" && form.psw.value == "openit") {
+                    window.open('/index')/*opens the target page while Id & password matches*/
+                }
+                else if(form.uname.value == "user" && form.psw.value == "login") {
+                    window.open('/index')/*opens the target page while Id & password matches*/
+                }
+                else if(form.uname.value == "username" && form.psw.value == "password") {
+                    window.open('/index')/*opens the target page while Id & password matches*/
+                }
+				
+			
+				
+				
+                else {
+                    alert("Please check your username and password")/*displays error message*/
+                }
+            }
