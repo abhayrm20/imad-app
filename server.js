21ec9143ac7990fbeca7ad88234a6f263a81a026
abhayrm20 <abhayrm20@gmail.com>
2017-08-05 21:39:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/vishal', function (req, res) {
+app.get('/ui/vishal', function (req, res) {
   res.send('Username: vishal Password: meanie');
  
 });
