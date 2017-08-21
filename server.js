576eae201376ef73a18f919c0a39613515122a3a
abhayrm20 <abhayrm20@gmail.com>
2017-08-21 23:18:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,19 @@ app.get('/hash/:input', function (req,res) {
    res.send(hashedString);
 });
 
+app.get('/create-user', function (req,res) {
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   Pool.query('INSERT INTO "user1" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+       if (err) {
+            res.status(500).send(err.toString());
+      }
+      else {
+            res.send('User successfully created:' + username);
+      }
+   });
+});
+
 var Pool = new Pool(config);
 
 app.get('/db-test', function (req, res) {
