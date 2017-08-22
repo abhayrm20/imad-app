ed4796c8878e7947a2e4c9e9e54c2e0f60c706e8
abhayrm20 <abhayrm20@gmail.com>
2017-08-22 23:23:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMPHttpRequest.DONE) {
+            if (request.status === 200) {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i< names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST', 'http://abhayrm01.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(JSON.stringify({username: username, password: password}));
+    
+};
