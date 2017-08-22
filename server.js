13d76e1710632a1d72da5c706e03b77139ca4640
abhayrm20 <abhayrm20@gmail.com>
2017-08-22 21:18:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/hash/:input', function (req,res) {
    res.send(hashedString);
 });
 
-app.post('/create-user', function (req,res) {
+app.get('/create-user', function (req,res) {
    var username = req.body.username;
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
