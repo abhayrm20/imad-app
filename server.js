385e889c4d2a1ba8188d11a700d33781ce92cd36
abhayrm20 <abhayrm20@gmail.com>
2017-08-22 21:39:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require ('pg').Pool;
-
+var crypto = require('crypto');
+var bodyParser = require('body-Parser');
 
 var config = {
     user: 'abhayrm01',
@@ -63,20 +64,7 @@ app.get('/hash/:input', function (req,res) {
    res.send(hashedString);
 });
 
-app.post('/create-user', function (req,res) {
-   var username = req.body.username;
-   var password = req.body.password;
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password, salt);
-   Pool.query('INSERT INTO "user1" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
-       if (err) {
-            res.status(500).send(err.toString());
-      }
-      else {
-            res.send('User successfully created:' + username);
-      }
-   });
-});
+
 
 var Pool = new Pool(config);
 
