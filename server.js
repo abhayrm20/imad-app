10b0177a884b7c888505280cd6cb0999856ac10e
abhayrm20 <abhayrm20@gmail.com>
2017-08-22 21:33:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,42 +1,150 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require ('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-Parser');
 
-
+var config = {
+    user: 'abhayrm01',
+    database: 'abhayrm01',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
-app.get('/traffic', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'timer.html'));
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate =`
+        <html>
+<head>
+    <title> ${title}</title>
+    <link href="/ui/style.css" rel = "stylesheet" />
+</head>
+<div class="container"> 
+    <div>
+        <h1>${heading}</h1>
+    </div>
+    <div>
+        <h3>${date.toDateString()}</h3>
+    </div>
+    <div>
+        ${content}
+    </div>
+</div>
+</html>`
+;
+return htmlTemplate;
+}
+
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+ 
 });
 
-app.get('/server', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'server.html'));
+function hash (input, salt) {
+  var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+  return ["pbkdf2Sync", "10000", salt, hashed.toString('hex')].join('$');
+  
+    
+}
+
+
+
+app.get('/hash/:input', function (req,res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+});
+
+app.post('/create-user', function (req,res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   Pool.query('INSERT INTO "user1" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+       if (err) {
+            res.status(500).send(err.toString());
+      }
+      else {
+            res.send('User successfully created:' + username);
+      }
+   });
+});
+
+var Pool = new Pool(config);
+
+app.get('/db-test', function (req, res) {
+   Pool.query('select * from article;', function(err,result) {
+      if (err) {
+            res.status(500).send(err.toString());
+      }
+      else {
+            res.send(JSON.stringify(result.rows));
+      }
+   
+   }); 
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
-});
-
-app.get('/index', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'login.css'));
+app.get('/', function (req, res) {
+  res.send(`Page loaded without errors`);
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/login.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.js'));
 });
 
 app.get('/guest', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'timer2.html'));
 });
 
-app.get('/hidden', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'hidden.html'));
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/articles/:articleName', function (req, res) {
+  Pool.query("select * from articles where term=$1", [req.params.articleName] , function(err,result){
+   if (err) {
+       res.status(500).send(err.toString());
+       }
+    else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        }
+        else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
+});
+
+app.get('/:articleName', function (req,res) {
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
+
+
+
+
+
+
 
 
 
