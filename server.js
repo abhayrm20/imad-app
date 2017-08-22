89a1607ce5717e8f616d7f467585bee3313eaa02
abhayrm20 <abhayrm20@gmail.com>
2017-08-22 21:44:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 
 function createTemplate (data) {
@@ -47,6 +47,35 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
  
+});
+
+function hash (input, salt) {
+  var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+  return ["pbkdf2Sync", "10000", salt, hashed.toString('hex')].join('$');
+  
+    
+}
+
+
+
+app.get('/hash/:input', function (req,res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+});
+
+app.post('/create-user', function (req,res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   Pool.query('INSERT INTO "user1" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+       if (err) {
+            res.status(500).send(err.toString());
+      }
+      else {
+            res.send('User successfully created:' + username);
+      }
+   });
 });
 
 
