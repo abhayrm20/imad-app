00105825c4241caba1cdb9333432dc5753a60bf2
abhayrm20 <abhayrm20@gmail.com>
2017-10-24 14:42:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -177,10 +177,10 @@ app.get('/articles/articleName', function (req, res) { //replace with /articles/
   });
 });
 
-/*app.get('/:articleName', function (req,res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});*/
+//app.get('/:articleName', function (req,res) {
+//    var articleName = req.params.articleName;
+//    res.send(createTemplate(articles[articleName]));
+//});
 
 
 
